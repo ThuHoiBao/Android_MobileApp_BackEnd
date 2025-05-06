c6491a738b84830f9b85eaab3bec2f9306968501
vuongducthoai <thoai12309@gmail.com>
2025-05-06 21:05:21 +0700

    Create API Review

--- AppMobile/src/main/java/com/userdb/mobileapp/repository/ReviewRepository.java
+++ AppMobile/src/main/java/com/userdb/mobileapp/repository/ReviewRepository.java
@@ -3,6 +3,7 @@ package com.userdb.mobileapp.repository;
 import com.userdb.mobileapp.entity.Product;
 import com.userdb.mobileapp.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
@@ -10,5 +11,12 @@ import java.util.Optional;
 
 public interface ReviewRepository  extends JpaRepository<Review, Integer> {
     Optional<Review> findByOrderItem_OrderItemID(int orderItemId);
+
+    @Query("SELECT r FROM Review r " +
+            "JOIN r.orderItem oi " +
+            "JOIN oi.product p " +
+            "JOIN r.orderItem.order o " +
+            "JOIN o.user u " +
+            "WHERE p.productName = :productName")
     List<Review> findReviewsByProductName(@Param("productName") String productName);
 }
